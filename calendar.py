b17e4e6d4cd93ef4029835904cecfd6addf192d4
Aleksander Grzegorzewski <aleksander@grzegorzew.ski>
2026-07-10 14:15:40 +0200

    updated calendar colors

--- calendar.py
+++ calendar.py
@@ -84,6 +84,7 @@ class PukRumiaPickupCalendar(CalendarEntity):
             manufacturer="sisms.pl",
             model="Unit timetable",
         )
+        self._attr_initial_color = self._bin.color
         self._event: CalendarEvent | None = None
         self._update_attrs()
 
